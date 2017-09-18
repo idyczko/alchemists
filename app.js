bb2b0a7670810c07686be8ecd087fe58abad041b
idyczko <igor.dyczko@gmail.com>
2017-09-18 21:46:54 +0200

    Request serving logic

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
