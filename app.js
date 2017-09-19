55cbdfd905e6672dd5ee4205de9dc70c48db502b
idyczko <igor.dyczko@gmail.com>
2017-09-19 23:13:46 +0200

    Postgre dependency

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+var pg = require('pg');
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
+});
+
+app.get('/db', function(req, res) {
+	pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+		client.query('SELECt * FROM test_table', function(err, result) {
+			done();
+			if(err){
+				console.error(err);
+				res.send("Error " + err);
+			} else {
+				res.render('db', {results: results.rows});
+			}
+		}))
+	})
 });
 
 app.use('/client', express.static(__dirname + '/client'));
