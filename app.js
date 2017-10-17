094a1bfd8ac8733c9038452612d44efe6fd3a642
idyczko <igor.dyczko@gmail.com>
2017-10-17 23:14:32 +0200

    Simple chat implemented

--- app.js
+++ app.js
@@ -11,8 +11,29 @@ app.use('/client', express.static(__dirname + '/client'));
 server.listen(process.env.PORT || 3000);
 console.log("Server has been started successfully...");
 
+var ACTIVE_SOCKETS = {};
+
 var io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket){
-	console.log('Opened socket connection: ');
-	console.log(socket);
+	socket.id = Math.random();
+	console.log("Opened socket connection: " +socket.id);
+	console.log();
+	ACTIVE_SOCKETS[socket.id] = socket;
+
+	socket.on('disconnect', function() {
+		console.log("Disconnecting socket " + socket.id);
+		delete ACTIVE_SOCKETS[socket.id];
+	});
+
+	socket.on("sendMessage", function(message) {
+		for(var i in ACTIVE_SOCKETS) {
+			ACTIVE_SOCKETS[i].emit("appendNewMessage", socket.id + ": " + message);
+		}
+	});
 });
+
+setInterval(function(){
+	for(var i in ACTIVE_SOCKETS){
+		ACTIVE_SOCKETS[i].emit('logId', {});
+	}
+}, 10000/25)
